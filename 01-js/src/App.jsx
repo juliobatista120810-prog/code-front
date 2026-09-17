f4cad2cc22dd39352818b4e33fb222c6f279675a
JULIO CESAR JAQUES BATISTA <julio_cj_batista@estudante.sesisenai.org.br>
2026-09-17 14:56:43 -0300

    A primeira parte está completa e salva

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -1,6 +1,44 @@
 import './App.css'
 
+//adicionar um botão toda vez que fizer um ex novo.
+
 function App() {
+
+function Mônika() {
+  let chance, n
+  
+  n = Number(prompt('Quantas vezes ele(a) usou o celular: '))
+  chance = Number(prompt('Quais são as chances de sucesso: '))
+
+  let chancesHumanos = (0.1/(1 + 500 * n)) * 100
+  
+  alert('Chances para humanos: ' + chancesHumanos)
+}
+
+function tellesTransportes() {
+  let peso, tara
+
+  peso = Number(prompt('Qual é o peso: '))
+  tara = Number(prompt('Qual é a tara: '))
+   
+
+  let carga = peso - tara
+
+  alert('Este é o peso da carga: ' + carga)
+}
+
+function junin() {
+  let salarioMensal, diasTrabalhados, salarioSemanal
+
+  salarioMensal = Number(prompt('Quanto você recebeu neste mês: '))
+  diasTrabalhados = Number(prompt('Quantos dias você trabalhou: '))
+  salarioSemanal = Number(prompt('Quanto você recebeu na semana: '))
+
+  let salarioDia = salarioMensal / diasTrabalhados;
+  salarioSemanal = salarioMensal * 5
+
+  alert('Isso é quanto você ganha por dia: ' + salarioDia, salarioSemanal)
+}
 
 function PeErnanBuco() {
   let custo, doacoes
@@ -72,6 +110,10 @@ function calcularPontos() {
     <button onClick={vendedorDePortoes}>vendedorDePortoes</button>
     <button onClick={trajetoPomar}>trajetoPomar</button>
     <button onClick={PeErnanBuco}>PeErnanBuco</button>
+    <button onClick={junin}>junin</button>
+    <button onClick={tellesTransportes}>tellesTransportes</button>
+    <button onClick={Mônika}>Mônika</button>
+
     <hr />
 
     <button onClick={testar}>Testar</button>
